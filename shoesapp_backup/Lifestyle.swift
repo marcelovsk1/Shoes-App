368db3d2763026e0e3a86ffb7399fc2383848886
Marcelo Amaral Alves <marceloaa@live.com>
2024-05-02 16:36:40 -0300

    working on

---
+++ shoesapp_backup/Lifestyle.swift
@@ -0,0 +1,18 @@
+//
+//  Lifestyle.swift
+//  shoesapp_backup
+//
+//  Created by Marcelo Amaral Alves on 2024-05-02.
+//
+
+import SwiftUI
+
+struct Lifestyle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Lifestyle()
+}
